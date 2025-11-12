b87ce129c31d89d73a97bb3f9bf5fb28da43f04c
kimwjdhyun <dkrehd7953@gmail.com>
2025-11-12 23:25:31 +0900

    연습문제 1~20

---
+++ 00_practice.py
@@ -0,0 +1,66 @@
+
+# print("Hello World!")
+
+# print("Mary's cosmetics")
+
+# print('신씨가 소리질렀다 "도둑이야"')
+
+# print("c:\windows")
+
+# print("안녕하세요. \n만나서\t\t반갑습니다")
+#'\t'는 탭을 의미, '\n'은 줄바꿈을 의미
+# print("오늘은", "일요일")
+
+# print('naver','kakao','sk','samsung' , sep=';')
+# sep= 인자로 출력되는 값들 사이에 공백 대신 출력할 값
+# print("naver", "kakao", "sk", "samsung", sep="/")
+
+# print("first", end='');print("second")
+# 세미콜론(;)은 한줄에 여러 개의 명령을 작성하기 위해 사용
+# print(5/3)
+'''
+삼성전자 = 50000
+총평가금액 = 삼성전자 * 10
+print(총평가금액)
+
+시가총액 = 298000000000000
+현재가 = 50000
+PER = 15.70
+print(시가총액, type(시가총액))
+print(현재가, type(현재가))
+print(PER, type(PER))
+
+'''
+'''
+s = "hello"
+t = "python"
+
+print(s+'!', t)
+'''
+'''
+a = "132"
+# print(type(a))
+# typr() 함수는 테이터 타입을 판별
+'''
+'''
+num_str = "720"
+num_int=int(num_str)
+print(num_int, type(num_int))
+'''
+# num = 100
+# num_str = str(num)
+# print(num_str, type(num_str))
+
+# num_str = "15.79"
+# num_float = float(num_str)
+# print(num_float, type(num_float))
+
+'''
+year = "2020"
+print(int(year)-3)
+print(int(year)-2)
+print(int(year)-1)
+'''
+month = 48584
+총금액 = month * 36
+print(총금액)
