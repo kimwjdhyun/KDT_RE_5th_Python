1c6881c239a9193e74b99501a364d6305a78b937
kimwjdhyun <dkrehd7953@gmail.com>
2025-11-13 21:56:46 +0900

    연습문제 20_40

--- 00_practice.py
+++ 00_practice.py
@@ -116,3 +116,35 @@ print(phone_number1) # 010 1234 5678
 string = 'abcdfe2a354a32a'
 string1 = string.replace("a", "A")
 print(string1)
+
+a = "3"
+b = "4"
+print(a + b) # 34
+
+a = '-'
+print(a*80)
+
+t1 = "python"
+t2 = "java"
+t3 = t1 + ' ' + t2 + ' '
+print(t3 * 4)
+
+name1 = "김민수" 
+age1 = 10
+name2 = "이철희"
+age2 = 13
+print(f'''"이름: {name1} 나이: {age1}
+ 이름: {name2} 나이: {age2}
+      ''')
+
+상장주식수 = "5,969,782,550"
+컴마제거 = 상장주식수.replace(",","")
+형변환 = int(컴마제거)
+print(형변환, type(형변환))
+
+분기 = "2020/03(E) (IFRS연결)"
+print(분기[:7])
+
+data = "   삼성전자    "
+data1 = data.strip()
+print(data1) # 문자열에서 strip() 사용시 좌우 공백 제거
