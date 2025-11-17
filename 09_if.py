d04ce9cf23bf4b2ff8c6122cb105dcff46fff233
kimwjdhyun <dkrehd7953@gmail.com>
2025-11-17 14:45:35 +0900

    if-elif-else문 정리 및 실습

--- 09_if.py
+++ 09_if.py
@@ -44,8 +44,105 @@
 
 # 실습 2. if -else 연습
 # 문제 1. 짝수 홀수 판별하기
-a = int(input("정수를 입력해 주세요. :"))
-if a%2 == 1:
-    print("홀수입니다.") # 36 / 짝수입니다.
-else:
-    print("짝수입니다.") # 9999 / 홀수입니다.
+# number = int(input("정수를 입력해 주세요. :"))
+# if number%2 == 1:
+#     print("홀수입니다.") # 36 / 짝수입니다.
+# else:
+#     print("짝수입니다.") # 9999 / 홀수입니다.
+
+
+# if -elif -else 문
+# elif : else if의 약자
+# elif에서 조건을 반드시 기록
+# if가 있어야만 사용 가능
+
+# score = int(input())
+# if score>=90: 
+#     print("A") # 99 / A
+# elif score >=80:
+#     print("B") # 85 / B
+# elif score >=70:
+#     print("C") # 73 / C
+# elif score>=60:
+#     print("D") # 61 / D
+# else:
+#     print("F") # 13 / F
+
+
+# 실습 3. if -elif -else문 연습
+# 문제1. 나이에 따른 영화 관람 가능 여부
+# age = int(input("관람 가능 등급 : "))
+# if age >= 19:
+#     print("청소년 관람불가 가능") # 28 / 청소년 관람불가 가능
+# elif age >= 16:
+#     print("15세 이상 관람가") # 17 / 15세 이상 관람가
+# elif age >= 13:
+#     print("12세 이상 관람가") # 14 / 12세 이상 관람가
+# else:
+#     print("전체 관람가") # 7 / 전체 관람가
+
+
+# 문제2. 시, 분, 초 구하기
+# time = int(input("초를 입력해주세요. :"))
+# second = time % 60
+# minute = time // 60
+# hour = time // 3600
+
+# if minute>time>=second:
+#     print(time, "초") 
+# elif hour>time>=minute:
+#     print(time // 3600, "분", ((time % 3600) % 60), "초")
+# elif time>=hour:
+#     print((time // 3600), "시간", ((time // 3600) // 60), "분", ((time % 3600) % 60), "초")
+
+#60s = 1m, 6-m = 1h
+# time = int(input("초를 입력하세요. :"))
+# minute = time // 60
+# second = time % 60
+# hour = minute // 60
+# minute %= 60
+
+# if hour>0:
+#     print(f"{hour}시간 {minute}분 {second}초")
+# elif minute > 0:
+#     print(f"{minute}분 {second}초")
+# else:
+#     print(f"{second}초")
+
+# 중첩 조건문
+# 하나의 if 문 안에 또 다른 if 문을 사용하는 것
+
+# user_name = input("관리자 아이디를 입력하세요. :")
+# password = input("비밀번호를 입력하세요. :")
+
+# if user_name == "admin":
+#     if password == "abcd":
+#         print("로그인 성공") # 관리자 아이디를 입력하세요. :admin 비밀번호를 입력하세요. :abcd 로그인 성공
+#     else:
+#         print("비밀번호가 잘못됐습니다.") # 관리자 아이디를 입력하세요. :admin 비밀번호를 입력하세요. :sdfa 비밀번호가 잘못됐습니다.
+# else:
+#     print("잘못된 사용자입니다.") # 관리자 아이디를 입력하세요. :ad 비밀번호를 입력하세요. :cdsd 잘못된 사용자입니다.
+
+# 실습3. 중첩 조건문 연습
+# 문제 1. 편의점 도시락 구매하기
+
+
+
+food = input("식품명을 입력하세요.")
+price = int(input("금액을 입력하세요."))
+
+if food == "김밥":
+    if price >= 2500:
+        print("구매가 완료되었습니다.")
+    else:
+        print("잔액이 부족합니다.")
+if food == "삼각김밥":
+    if price >= 1500:
+        print("구매가 완료되었습니다.")
+    else:
+        print("잔액이 부족합니다.")
+if food == "도시락":
+    if price >= 4000:
+        print("구매가 완료되었습니다.")
+    else:
+        print("잔액이 부족합니다.")
