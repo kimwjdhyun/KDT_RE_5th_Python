06f89906775a332752e8509d9f52288415f4644d
kimwjdhyun <dkrehd7953@gmail.com>
2025-11-20 10:05:15 +0900

    while문 마무리

--- 00_practice.py
+++ 00_practice.py
@@ -419,3 +419,17 @@ while True:     # 전체 프로그램 반복
             print("잘못 입력하셨습니다.\n")
             continue
         '''
+'''
+start, stop = map(int, input().split())
+ 
+i = start
+ 
+while True:
+    if i > stop:
+        break
+    if i % 10 == 3:
+        i += 1
+        continue
+    print(i, end=' ')
+    i += 1
+'''
