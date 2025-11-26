f0330381dcf7ecef87819076f590bbc6c390bd8d
kimwjdhyun <dkrehd7953@gmail.com>
2025-11-26 09:46:12 +0900

    랜덤 모듈 사용 예제 추가 및 거리 계산 실습 주석 처리

--- 14_module.py
+++ 14_module.py
@@ -118,25 +118,98 @@ classroom(18, 24)
 # 실습2.
 # 📌 문제 1. 실제 거리 계산: 좌표 두 점 사이 거리 구하기
 
-x1, y1 = map(int, input("x1,y1을 입력해주세요.").split(","))
-# x1, y1 = int(x1), int(y1)
-x2, y2 = map(int, input("x2,y2을 입력해주세요.").split(","))
+# x1, y1 = map(int, input("x1,y1을 입력해주세요.").split(","))
+# # x1, y1 = int(x1), int(y1)
+# x2, y2 = map(int, input("x2,y2을 입력해주세요.").split(","))
 
-# 피타고라스 정리: 거리 = sqrt((x2-x1)^2 + (y2-y1)^2)
-dist = round(m.sqrt(m.pow((x2-x1), 2) + m.pow((y2-y1), 2)), 2)
+# # 피타고라스 정리: 거리 = sqrt((x2-x1)^2 + (y2-y1)^2)
+# dist = round(m.sqrt(m.pow((x2-x1), 2) + m.pow((y2-y1), 2)), 2)
 
-print(f"두 점 사이의 거리는: {dist}")
+# print(f"두 점 사이의 거리는: {dist}")
 
 
-# 📌 문제 2. 상품 나누기: 최소 공배수와 최대 공약수
-a = 18
-b = 24
+# # 📌 문제 2. 상품 나누기: 최소 공배수와 최대 공약수
+# a = 18
+# b = 24
 
-# 최대공약수
-gcd = m.gcd(a, b)
+# # 최대공약수
+# gcd = m.gcd(a, b)
 
-# 최소공배수
-lcm = m.lcm(a, b)
+# # 최소공배수
+# lcm = m.lcm(a, b)
 
-print(f"최대 간식 개수: {gcd}")
-print(f"최소 간식 개수: {lcm}")
+# print(f"최대 간식 개수: {gcd}")
+# print(f"최소 간식 개수: {lcm}")
+
+# random 모듈 : 랜덤 값(난수) 생성 시 사용
+import random as r
+
+# 1. 난수 생성
+# random() : 0.0 이상 1.0 미만의 실수(float) 난수 반환
+print(r.random())           # 0.0 ~ 1.0 미만의 실수 난수 반환
+
+# uniform(a, b) : a 이상 b 미만의 실수 난수 반환
+print(r.uniform(1, 10))    # 1.0 ~ 10.0 미만의 실수 난수 반환
+
+# randint(a, b) : a 이상 b 이하의 정수(int) 난수 반환
+print(r.randint(1, 100))    # 1 ~ 10 이하의 정수 난수 반환
+
+# randrange(start, stop, step) : 범위 안의 정수 난수 반환, 간격 지정 가능
+print(r.randrange(0, 101, 5))  # 1 ~ 100 이하의 홀수 정수 난수 반환
+
+
+# 2. 랜덤 선택
+fruits = ['apple', 'banana', 'watermelon', 'grape', "orenge"]
+
+# choice(seq) : 시퀀스)에서 임의의 요소 1개 반환
+print(r.choice(fruits))     # fruits 리스트에서 임의의 요소 1개 반환
+
+# choices(seq, k) : 시퀀스에서 "중복을 허용해서" k개 요소 리스트 반환
+print(r.choices(fruits, k=2))  # fruits 리스트에서 임의의 요소 2개 반환
+
+# 섞기
+# sample(seq, k) : 시퀀스에서 "중복 없이" k개 요소 리스트 반환
+print(r.sample(fruits, k=2))   # fruits 리스트에서 임의의 요소 2개 반환
+
+# shuffle(seq) : 시퀀스의 요소들을 무작위로 섞음 -> 원본 시퀀스를 변경
+numbers = [1, 2, 3, 4, 5]
+print(r.shuffle(numbers))  # None
+print(numbers)             # 섞인 numbers 리스트 출력
+
+# 실습 1. 로또 번호 뽑기
+import random as r
+
+def lotto(num):
+    return r.sample(range(1, 46), k=num)
+print(sorted(lotto(6)))  
+# [11, 17, 23, 27, 37, 39]
+# [1, 5, 11, 22, 31, 41]
+# [8, 9, 21, 23, 26, 39]
+# [10, 12, 14, 23, 32, 45]
+
+# 실습 2. 가위 바위 보 게임 만들기
+import random as r 
+
+user = input("무엇을 낼지 골라주세요 : ")
+
+def game(user):
+    computer = r.choice(["가위", "바위", "보"])
+    if user == computer:
+        return " 무승부 "
+    elif user == "가위" and computer == "보":
+        return " 승리! "
+    elif user == "바위" and computer == "가위":
+        return " 승리! "
+    elif user == "보" and computer == "바위":
+        return " 승리! "
+    else:
+        return " 패배! "
+print(game(user))   
+# 무엇을 낼지 골라주세요 : 보
+#  무승부
+# 무엇을 낼지 골라주세요 : 바위
+#  승리!
+# 무엇을 낼지 골라주세요 : 가위
+#  승리!
+# 무엇을 낼지 골라주세요 : 가위
+#  패배!
